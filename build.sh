47e504e41cb4f032a325b197427ea119e4c6b372
guillaum1 <guillaum1@dbcedcf8-ba24-0410-91cf-f9b3ddd91ace>
2007-04-07 23:16:39 +0000

    Fixed bug occurring when creating midpath.jar
    git-svn-id: svn://svn.code.sf.net/p/midpath/code/trunk@85 dbcedcf8-ba24-0410-91cf-f9b3ddd91ace

--- build.sh
+++ build.sh
@@ -25,17 +25,17 @@ CLDC_PATH=$DIST_HOME/dist/cldc1.1.jar
 
 # Build SDLJava for J2SE
 cd $DIST_HOME/external/sdljava/java
-make jar JAVAC=$JAVAC_CMD JAR_FILE="sdljava-j2se.jar" JAVAC_FLAGS="-bootclasspath $GNU_CLASSPATH_PATH"
+make jar JAVAC=$JAVAC_CMD JAR_FILE="sdljava-j2se.jar" JAVAC_FLAGS="-bootclasspath $GNU_CLASSPATH_PATH" JAR_FLAGS="cvf" 
 cp $DIST_HOME/external/sdljava/java/sdljava-j2se.jar $DIST_HOME/dist
 
 # Build SDLJava for CLDC
 cd $DIST_HOME/external/sdljava-cldc
-make jar JAVAC=$JAVAC_CMD JAR_FILE="sdljava-cldc.jar" JAVAC_FLAGS="-bootclasspath $CLDC_PATH:$GNU_CLASSPATH_PATH -source 1.3 -target 1.1"
+make jar JAVAC=$JAVAC_CMD JAR_FILE="sdljava-cldc.jar" JAVAC_FLAGS="-bootclasspath $CLDC_PATH:$GNU_CLASSPATH_PATH -source 1.3 -target 1.1" JAR_FLAGS="cvf"
 cp $DIST_HOME/external/sdljava-cldc/sdljava-cldc.jar $DIST_HOME/dist
 
 # Build Escher X11 library
 cd $DIST_HOME/external/escher-cldc/core
-make jar JAVAC=$JAVAC_CMD JAR_FILE="escher-x11-cldc.jar" JAVAC_FLAGS="-bootclasspath $CLDC_PATH -source 1.3 -target 1.1"
+make jar JAVAC=$JAVAC_CMD JAR_FILE="escher-x11-cldc.jar" JAVAC_FLAGS="-bootclasspath $CLDC_PATH -source 1.3 -target 1.1" JAR_FLAGS="cvf"
 cp $DIST_HOME/external/escher-cldc/core/escher-x11-cldc.jar $DIST_HOME/dist
 
 # Build the SDLJava native part
@@ -45,8 +45,10 @@ cp *.so $DIST_HOME/dist
 
 # Build MIDPath
 cd $DIST_HOME/src/core
-make jar JAVAC=$JAVAC_CMD JAR_FILE="midpath.jar" JAVAC_FLAGS="-bootclasspath $CLDC_PATH:$GNU_CLASSPATH_PATH:$DIST_HOME/dist/sdljava-cldc.jar:$DIST_HOME/dist/escher-x11-cldc.jar:$DIST_HOME/lib/kxml2-2.3.0.jar:$DIST_HOME/lib/swt.jar -source 1.3 -target 1.1"
-cp $DIST_HOME/src/core/midpath.jar $DIST_HOME/dist
+make install JAVAC=$JAVAC_CMD CLASS_DIR=$DIST_HOME/src/core/classes JAVAC_FLAGS="-bootclasspath $CLDC_PATH:$GNU_CLASSPATH_PATH:$DIST_HOME/dist/sdljava-cldc.jar:$DIST_HOME/dist/escher-x11-cldc.jar:$DIST_HOME/lib/kxml2-2.3.0.jar:$DIST_HOME/lib/swt.jar -source 1.3 -target 1.1"
+cd $DIST_HOME/src/core/classes
+jar cvf $DIST_HOME/dist/midpath.jar *
+#cp $DIST_HOME/src/core/midpath.jar $DIST_HOME/dist
 
 # Add resources to the midpath.jar
 #jar uvf $DIST_HOME/dist/midpath.jar -C $DIST_HOME/resources-embedded/ .
