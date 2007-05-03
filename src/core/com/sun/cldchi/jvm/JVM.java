ce1d43a00037b83e4f8ce302d3bec4dde94ad8c9
guillaum1 <guillaum1@dbcedcf8-ba24-0410-91cf-f9b3ddd91ace>
2007-05-03 21:44:09 +0000

    Made JNI library loading compliant with J2SE conventions
    git-svn-id: svn://svn.code.sf.net/p/midpath/code/trunk@110 dbcedcf8-ba24-0410-91cf-f9b3ddd91ace

--- src/core/com/sun/cldchi/jvm/JVM.java
+++ src/core/com/sun/cldchi/jvm/JVM.java
@@ -8,7 +8,16 @@ public class JVM {
 	 * @param name The name of the library to load
 	 */
 	public static void loadLibrary(String name) {
+		
+		if(name.startsWith("lib") && (name.length() > 3)) {
+			name = name.substring(3, name.length());
+		}
+		
+		if(name.endsWith(".so") && (name.length() > 3)) {
+			name = name.substring(0, name.length() - 3);
+		}
+		
 		System.loadLibrary(name);
 	}
-
+	
 }
