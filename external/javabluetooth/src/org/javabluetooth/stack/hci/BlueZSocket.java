92806e6ed0ca0281cbcc7f8887aa46a22ec63682
guillaum1 <guillaum1@dbcedcf8-ba24-0410-91cf-f9b3ddd91ace>
2007-06-18 16:05:25 +0000

    Added BlueZ socket binding
    git-svn-id: svn://svn.code.sf.net/p/midpath/code/trunk@137 dbcedcf8-ba24-0410-91cf-f9b3ddd91ace

--- external/javabluetooth/src/org/javabluetooth/stack/hci/BlueZSocket.java
+++ external/javabluetooth/src/org/javabluetooth/stack/hci/BlueZSocket.java
@@ -1,48 +1,46 @@
 package org.javabluetooth.stack.hci;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class BlueZSocket {
-	
-	protected boolean isOpen = false;
-	
-	public void open(String deviceName, int bufferSize, int sampleRate) throws IOException {
 
-		int rval = open0(deviceName, bufferSize, sampleRate);
+	private int handle;
+	private boolean connectionOpen = false;
+	private BlueZInputStream inputStream = new BlueZInputStream();
+	private BlueZOutputStream outputStream = new BlueZOutputStream();
 
-		if (rval < 0) {
-			throw new IOException("Can't open device. Reason: " + rval);
-		}
-	}
-	
-	public int available() throws IOException {
-		int available = available0();
-
-		if (available < 0) {
-			throw new IOException();
-		}
-		
-		return available;
-		
+	public void open(int deviceNumber) throws IOException {
+		handle = open0(deviceNumber);
+		connectionOpen = true;
 	}
 
 	public void close() throws IOException {
-		close0();
-		isOpen = false;
+		if (connectionOpen) {
+			close0(handle);
+			connectionOpen = false;
+		}
 	}
 	
-	public boolean isOpen() {
-		return isOpen;
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+	
+	public OutputStream getOutputStream() {
+		return outputStream;
 	}
 
-	public void write(byte[] buf, int offset, int len) throws IOException {
-		while (len > 0) {
-			int writtenBytes = write0(buf, offset, len);
-			if (writtenBytes < 0) {
-				throw new IOException();
-			}
-			offset += writtenBytes;
-			len -= writtenBytes;
+	public boolean isOpen() {
+		return connectionOpen;
+	}
+
+	/**
+	 * Ensure connection is open
+	 */
+	private void ensureOpen() throws IOException {
+		if (!connectionOpen) {
+			throw new IOException("Connection closed");
 		}
 	}
 	
@@ -57,7 +55,7 @@ public class BlueZSocket {
 	 * @param sampleRate the sample rate
 	 * @return a value > 0 if success, otherwise a value < 0
 	 */
-	private native int open0(String deviceName, int bufferSize, int sampleRate);
+	private native int open0(int deviceNumber);
 
 	/**
 	 * Obtains the number of bytes of data that can be written to the buffer
@@ -65,7 +63,7 @@ public class BlueZSocket {
 	 * 
 	 * @return the amount of data available (in bytes) or a negative value if an error occurred
 	 */
-	private native int available0();
+	private native int available0(int handle);
 
 	/**
 	 * Writes data to the socket
@@ -75,11 +73,184 @@ public class BlueZSocket {
 	 * @param len number of bytes to write to the line
 	 * @return the number of bytes actually written or a negative value if an error occurred
 	 */
-	private native int write0(byte[] buf, int offset, int frames);
+	private native int write0(int handle, byte[] b, int offset, int frames);
+
+	private native int read0(int handle, byte[] b, int offset, int len);
 
 	/**
 	 * Closes the socket
 	 */
-	private native void close0();
+	private native void close0(int handle);
 
+	
+	class BlueZInputStream extends InputStream {
+		
+	    private boolean eof = false;
+	    private byte[] inBuffer = new byte[1];
+
+		public int read() throws IOException {
+			ensureOpen();
+	        if (eof) {
+	            return -1;
+	        }
+	        int res = read0(handle, inBuffer, 0, 1);
+	        if (res == -1) {
+	            eof = true;
+	        }
+	        return inBuffer[0];
+		}
+		
+		public int read(byte[] b) throws IOException { 
+			return read(b, 0, b.length);
+		}
+		
+		 /**
+	     * Reads up to <code>len</code> bytes of data from the input stream into
+	     * an array of bytes.
+	     * <p>
+	     * Polling the native code is done here to allow for simple
+	     * asynchronous native code to be written. Not all implementations
+	     * work this way (they block in the native code) but the same
+	     * Java code works for both.
+	     *
+	     * @param      b     the buffer into which the data is read.
+	     * @param      off   the start offset in array <code>b</code>
+	     *                   at which the data is written.
+	     * @param      len   the maximum number of bytes to read.
+	     * @return     the total number of bytes read into the buffer, or
+	     *             <code>-1</code> if there is no more data because the end of
+	     *             the stream has been reached.
+	     * @exception  IOException  if an I/O error occurs.
+	     */
+	    synchronized public int read(byte b[], int off, int len)
+	            throws IOException {
+	        ensureOpen();
+	        if (eof) {
+	            return -1;
+	        }
+	        if (len == 0) {
+	            return 0;
+	        }
+	        // Check for array index out of bounds, and NullPointerException,
+	        // so that the native code doesn't need to do it
+	        int test = b[off] + b[off + len - 1];
+
+	        int n = 0;
+	        while (n < len) {
+	            int count = read0(handle, b, off + n, len - n);
+	            if (count == -1) {
+	                eof = true;
+	                if (n == 0) {
+	                    n = -1;
+	                }
+	                break;
+	            }
+	            n += count;
+	            if (n == len) {
+	                break;
+	            }
+	        }
+	        
+	        return n;
+	    }
+	    
+	    public int available() throws IOException {
+			ensureOpen();
+			return available0(handle);
+		}
+	    
+	    /**
+	     * Close the stream.
+	     *
+	     * @exception  IOException  if an I/O error occurs
+	     */
+	    public void close() throws IOException {
+	        BlueZSocket.this.close();
+	    }
+		
+	}
+	
+	/**
+	 * Output stream for the connection
+	 */
+	class BlueZOutputStream extends OutputStream {
+
+		private byte[] outBuffer = new byte[1];
+		
+	    /**
+	     * Writes the specified byte to this output stream.
+	     * <p>
+	     * Polling the native code is done here to allow for simple
+	     * asynchronous native code to be written. Not all implementations
+	     * work this way (they block in the native code) but the same
+	     * Java code works for both.
+	     *
+	     * @param      b   the <code>byte</code>.
+	     * @exception  IOException  if an I/O error occurs. In particular,
+	     *             an <code>IOException</code> may be thrown if the
+	     *             output stream has been closed.
+	     */
+	    synchronized public void write(int b) throws IOException {
+	        ensureOpen();
+	        outBuffer[0] = (byte)b;
+	        while (true) {
+	            int res = write0(handle, outBuffer, 0, 1);
+	            if (res != 0) {
+	                return;
+	            }
+	        }
+	    }
+	    
+	    synchronized public void write(byte[] b) throws IOException {
+	    	write(b, 0, b.length);
+	    }
+
+	    /**
+	     * Writes <code>len</code> bytes from the specified byte array
+	     * starting at offset <code>off</code> to this output stream.
+	     * <p>
+	     * Polling the native code is done here to allow for simple
+	     * asynchronous native code to be written. Not all implementations
+	     * work this way (they block in the native code) but the same
+	     * Java code works for both.
+	     *
+	     * @param      b     the data.
+	     * @param      off   the start offset in the data.
+	     * @param      len   the number of bytes to write.
+	     * @exception  IOException  if an I/O error occurs. In particular,
+	     *             an <code>IOException</code> is thrown if the output
+	     *             stream is closed.
+	     */
+	    synchronized public void write(byte b[], int off, int len)
+	            throws IOException {
+	        ensureOpen();
+	        if (len == 0) {
+	            return;
+	        }
+
+	        // Check for array index out of bounds, and NullPointerException,
+	        // so that the native code doesn't need to do it
+	        int test = b[off] + b[off + len - 1];
+
+	        int n = 0;
+	        while (true) {
+	            n += write0(handle, b, off + n, len - n);
+	            if (n == len) {
+	                break;
+	            }
+	        }
+	    }
+
+	    /**
+	     * Close the stream.
+	     *
+	     * @exception  IOException  if an I/O error occurs
+	     */
+	    public void close() throws IOException {
+	        BlueZSocket.this.close();
+	    }
+
+	}
+
+	
 }
--- external/javabluetooth/src/org/javabluetooth/stack/hci/BlueZTransport.java
+++ external/javabluetooth/src/org/javabluetooth/stack/hci/BlueZTransport.java
@@ -1,14 +1,86 @@
 package org.javabluetooth.stack.hci;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.javabluetooth.util.Debug;
+
 public class BlueZTransport extends HCIDriver {
 
-	protected void reset() throws HCIException {
-		// TODO Auto-generated method stub
+	private byte[] readBuffer = new byte[32];
+	private BlueZSocket socket;
+	private int deviceNumber;
+	private InputStream inputStream;
+	private OutputStream outputStream;
+	private PollThread thread = new PollThread();
 
+	public BlueZTransport(int deviceNumber) throws HCIException {
+		this.deviceNumber = deviceNumber;
+		reset();
+	}
+	
+	protected void close() throws IOException {
+		if (socket != null) {
+			socket.close();
+		}
+		if (thread != null) {
+			thread.stop();
+		}
+	}
+
+	protected void reset() throws HCIException {
+		try {
+			close();
+			socket = new BlueZSocket();
+			socket.open(deviceNumber);
+			inputStream = socket.getInputStream();
+			outputStream = socket.getOutputStream();
+			thread = new PollThread();
+			thread.start();
+		} catch (IOException e) {
+			throw new HCIException("Can't open device hci" + deviceNumber);
+		}
 	}
 
 	protected void sendPacket(byte[] packet) throws HCIException {
-		// TODO Auto-generated method stub
+		try {
+			Debug.println(0, "HCI: Sending Packet:", packet);
+			outputStream.write(packet);
+		} catch (IOException e) {
+			throw new HCIException("IO Error while sending Packet. " + e);
+		}
+	}
+
+	private class PollThread implements Runnable {
+
+		private volatile Thread thread;
+
+		public void start() {
+			thread = new Thread(PollThread.this);
+		}
+
+		public void stop() {
+			thread = null;
+		}
+
+		public void run() {
+
+			try {
+				while (thread == Thread.currentThread()) {
+					int byteCount = inputStream.read(readBuffer);
+					if (byteCount > 0) {
+						receiveData(readBuffer, byteCount);
+					} else {
+						// EOF
+						break;
+					}
+				}
+			} catch (IOException e) {
+				Debug.println(Debug.DEBUGLEVELMAX, e.getMessage());
+			}
+
+		}
 
 	}
 
