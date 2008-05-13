5183d814111cf4d8660a9e09638396e0e88575a2
guillaum1 <guillaum1@dbcedcf8-ba24-0410-91cf-f9b3ddd91ace>
2008-05-13 13:51:10 +0000

    * bin/midpath-demo.sh: New script to start the demos
    * bin/midpath-midlet_launcher.sh: Modified to be called as a real launch script
    * bin/midpath-midlet_launcher-cacao_cldc.sh: Likewise
    
    * configuration/com/sun/midp/configuration/system_properties: Removed ^M
    
    
    git-svn-id: svn://svn.code.sf.net/p/midpath/code/trunk@262 dbcedcf8-ba24-0410-91cf-f9b3ddd91ace

--- bin/midpath-midlet_launcher-cacao_cldc.sh
+++ bin/midpath-midlet_launcher-cacao_cldc.sh
@@ -3,27 +3,39 @@
 # Adapt the next line to your environment
 JAVA_CMD=cacao
 
-if [ "$MIDPATH_HOME" = "" ]; then
+if [ ! $MIDPATH_HOME ]; then
   MIDPATH_HOME=$(pwd)/..
 fi
 
+if [ $# -lt 2 ]; then
+  echo "Usage :"
+  echo "  $(basename $0) <classpath> <midlet-class> [midlet-name]"
+  echo "  $(basename $0) -jar <jar-file>"
+  exit 1
+fi 
+
 # Set the classpath
-BCP=$MIDPATH_HOME/dist/cldc1.1.jar:$MIDPATH_HOME/dist/midpath.jar:$MIDPATH_HOME/configuration:$MIDPATH_HOME/dist/microbackend.jar:$MIDPATH_HOME/dist/sdljava-cldc.jar:$MIDPATH_HOME/dist/escher-x11-cldc.jar:$MIDPATH_HOME/dist/kxml2-2.3.0.jar:$MIDPATH_HOME/dist/jlayerme-cldc.jar:$MIDPATH_HOME/dist/jorbis-cldc.jar:$MIDPATH_HOME/dist/avetanabt-cldc.jar
-# Add the  MIDlet jar to the classpath  (must be loaded by the main 
-# classloader yet)
-BCP=$BCP:$MIDPATH_HOME/dist/midpath-tests.jar
+BCP=$MIDPATH_HOME/dist/cldc1.1.jar:$MIDPATH_HOME/dist/midpath.jar:$MIDPATH_HOME/configuration:$MIDPATH_HOME/dist/kxml2-2.3.0.jar:$MIDPATH_HOME/dist/microbackend.jar:$MIDPATH_HOME/dist/jlayerme-cldc.jar:$MIDPATH_HOME/dist/jorbis-cldc.jar:$MIDPATH_HOME/dist/escher-cldc.jar:$MIDPATH_HOME/dist/sdljava-cldc.jar:$MIDPATH_HOME/dist/avetanabt-cldc.jar:$MIDPATH_HOME/dist/jsr172-jaxp.jar:$MIDPATH_HOME/dist/jsr172-jaxrpc.jar:$MIDPATH_HOME/dist/jsr179-location.jar:$MIDPATH_HOME/dist/jsr184-m3g.jar:$MIDPATH_HOME/dist/jsr205-messaging.jar:$MIDPATH_HOME/dist/jsr226-svg-core.jar:$MIDPATH_HOME/dist/jsr226-svg-midp2.jar:$MIDPATH_HOME/dist/jsr226-svg-awt.jar:$MIDPATH_HOME/dist/jsr239-opengles-core.jar:$MIDPATH_HOME/dist/jsr239-opengles-jgl.jar:$MIDPATH_HOME/dist/jsr239-opengles-nio.jar:$MIDPATH_HOME/dist/jgl-cldc.jar
+
+# Parse the arguments
+if [ $1 = "-jar" ]; then
+ CP=$CP:$2
+ ARGS="$1 $2"
+else
+ CP=$CP:$1
+ ARGS="$2 $3"
+fi
 
 # Path of the native libraries
 JLP=$MIDPATH_HOME/dist
 export LD_LIBRARY_PATH=$JLP 
 
-SYSTEM_PROPERTIES="-Djavax.microedition.io.Connector.protocolpath=com.sun.midp.io -Dfile.separator=/"
+# System properties required by MIDP2 and JSR specs
+source $MIDPATH_HOME/configuration/com/sun/midp/configuration/system_properties
+SYSTEM_PROPERTIES=$SYSTEM_PROPERTIES:" -Djavax.microedition.io.Connector.protocolpath=com.sun.midp.io -Dfile.separator=/"
 
 CLASS=org.thenesis.midpath.main.MIDletLauncher
-# The classname of the MIDlet (we have to set it manually yet)
-MIDLET=org.thenesis.midpath.test.HelloWorldMidlet
-#MIDLET=org.thenesis.midpath.test.TextFieldTest
 
-# Note: "-Xbootclasspath/c" argument prepends the CLDC classes (defined at cacao configure time) and vm.zip (created at cacao compile time) to the bootclasspath. Use "-Xbootclasspath" instead if you need to override these paths.
-$JAVA_CMD -Xbootclasspath/c:${BCP} ${SYSTEM_PROPERTIES} ${CLASS} ${MIDLET}
+# Note: "-Xbootclasspath/c" argument prepends vm.zip (created at cacao compile time) to the bootclasspath. Use "-Xbootclasspath" instead if you need to override it.
+$JAVA_CMD -Xbootclasspath/c:${BCP} "${SYSTEM_PROPERTIES}" ${CLASS} ${ARGS}
 
--- bin/midpath-midlet_launcher.sh
+++ bin/midpath-midlet_launcher.sh
@@ -3,22 +3,36 @@
 # Modify the next line to change the JVM
 JAVA_CMD=java
 
-if [ "$MIDPATH_HOME" = "" ]; then
+if [ ! $MIDPATH_HOME ]; then
   MIDPATH_HOME=$(pwd)/..
 fi
 
+if [ $# -lt 2 ]; then
+ echo "Usage :"
+ echo "  $(basename $0) <classpath> <midlet-class> [midlet-name]" 
+ echo "  $(basename $0) -jar <jar-file>"
+ exit 1
+fi
+
 # Set the classpath
-CP=$MIDPATH_HOME/dist/midpath.jar:$MIDPATH_HOME/configuration:$MIDPATH_HOME/dist/microbackend.jar:$MIDPATH_HOME/dist/sdljava-cldc.jar:$MIDPATH_HOME/dist/escher-x11-cldc.jar:$MIDPATH_HOME/dist/kxml2-2.3.0.jar:$MIDPATH_HOME/dist/jlayerme-cldc.jar:$MIDPATH_HOME/dist/jorbis-cldc.jar:$MIDPATH_HOME/dist/avetanabt-cldc.jar
-# Add the  MIDlet jar to the classpath  (must be loaded by the main classloader)
-CP=$CP:$MIDPATH_HOME/dist/midpath-tests.jar
+CP=$MIDPATH_HOME/dist/midpath.jar:$MIDPATH_HOME/configuration:$MIDPATH_HOME/dist/kxml2-2.3.0.jar:$MIDPATH_HOME/dist/microbackend.jar:$MIDPATH_HOME/dist/jlayerme-cldc.jar:$MIDPATH_HOME/dist/jorbis-cldc.jar:$MIDPATH_HOME/dist/escher-cldc.jar:$MIDPATH_HOME/dist/sdljava-cldc.jar:$MIDPATH_HOME/dist/avetanabt-cldc.jar:$MIDPATH_HOME/dist/jsr172-jaxp.jar:$MIDPATH_HOME/dist/jsr172-jaxrpc.jar:$MIDPATH_HOME/dist/jsr179-location.jar:$MIDPATH_HOME/dist/jsr184-m3g.jar:$MIDPATH_HOME/dist/jsr205-messaging.jar:$MIDPATH_HOME/dist/jsr226-svg-core.jar:$MIDPATH_HOME/dist/jsr226-svg-midp2.jar:$MIDPATH_HOME/dist/jsr226-svg-awt.jar:$MIDPATH_HOME/dist/jsr239-opengles-core.jar:$MIDPATH_HOME/dist/jsr239-opengles-jgl.jar:$MIDPATH_HOME/dist/jsr239-opengles-nio.jar:$MIDPATH_HOME/dist/jgl-cldc.jar
+
+# Parse the arguments
+if [ $1 = "-jar" ]; then
+ CP=$CP:$2
+ ARGS="$1 $2"
+else
+ CP=$CP:$1
+ ARGS="$2 $3"
+fi
 
 # Path of the native libraries
 JLP=$MIDPATH_HOME/dist
 
-CLASS=org.thenesis.midpath.main.MIDletLauncher
-# The classname of the MIDlet
-#MIDLET=org.thenesis.midpath.test.HelloWorldMidlet
-MIDLET=org.thenesis.midpath.test.TextFieldTest
+# System properties required by MIDP2 and JSR specs
+source $MIDPATH_HOME/configuration/com/sun/midp/configuration/system_properties
 
-$JAVA_CMD -Djava.library.path=${JLP} -cp ${CP} ${CLASS} ${MIDLET}
+CLASS=org.thenesis.midpath.main.MIDletLauncher
+
+$JAVA_CMD -Djava.library.path=${JLP} "${SYSTEM_PROPERTIES}" -cp ${CP} ${CLASS} ${ARGS}
 
